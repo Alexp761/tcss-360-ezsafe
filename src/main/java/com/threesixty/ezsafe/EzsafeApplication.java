c2b63e8f82b99fde52c95f26b05576e30a5551d8
Alec Mac <alecmac95@gmail.com>
2021-02-12 22:36:45 -0800

    initial commit

---
+++ src/main/java/com/threesixty/ezsafe/EzsafeApplication.java
@@ -0,0 +1,13 @@
+package com.threesixty.ezsafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzsafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EzsafeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/threesixty/ezsafe/EzsafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.threesixty.ezsafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzsafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
