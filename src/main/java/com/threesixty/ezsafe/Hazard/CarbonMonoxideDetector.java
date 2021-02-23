b35749fb5a155f528d9c55cc22030a172f37a8a7
Alec Mac <alecmac95@gmail.com>
2021-02-22 20:27:46 -0800

    added small change to CO detector

--- src/main/java/com/threesixty/ezsafe/Hazard/CarbonMonoxideDetector.java
+++ src/main/java/com/threesixty/ezsafe/Hazard/CarbonMonoxideDetector.java
@@ -1,6 +1,6 @@
 package com.threesixty.ezsafe.Hazard;
 import com.threesixty.ezsafe.Device;
 
-public class CarbonMonoxideDetector extend Device{
+public class CarbonMonoxideDetector extend Device {
 
 }
