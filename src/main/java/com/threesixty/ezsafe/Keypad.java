f02d87d64971d0a18767c08c7a133a6d5393c9ee
Alexander Perez <alexperez761@gmail.com>
2021-02-26 02:48:44 -0800

    updated Keypad and KeychainRemote classes

--- src/main/java/com/threesixty/ezsafe/Keypad.java
+++ src/main/java/com/threesixty/ezsafe/Keypad.java
@@ -1,5 +1,34 @@
 package com.threesixty.ezsafe;
 
 public class Keypad {
-
+	private String armState;
+	private String deviceID;
+	private boolean deviceState;
+	private boolean loginState;
+	
+	
+    public boolean acceptPIN(int pin) {
+    }
+    
+    
+    public boolean getArmedStatus() {
+    	return this.armState; 
+    }
+    
+    
+    public void armStay() {
+    	this.armState = "Stay";
+    }
+    
+    public void armAway() {
+    	this.armState = "Away";
+    }
+    
+    public void disarm() {
+    	this.armState = "Off";
+    }
+    
+    public void requestEmergencyService(final String Service) {
+    	
+    }
 }
