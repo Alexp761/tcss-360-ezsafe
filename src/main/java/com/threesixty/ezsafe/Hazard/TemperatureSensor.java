1fc3db458e8fe39c30a5b88ed9b693b968c0fde7
michaeltademu <michaeltademu@localhost>
2021-03-01 19:26:15 -0800

    chnaged temprature sensor

--- src/main/java/com/threesixty/ezsafe/Hazard/TemperatureSensor.java
+++ src/main/java/com/threesixty/ezsafe/Hazard/TemperatureSensor.java
@@ -1,5 +1,39 @@
 package com.threesixty.ezsafe.Hazard;
 
-public class TemperatureSensor {
+// be able to set temprature of the house 
+// set the temprature of separte rooms 
+// classify the temprature level 
+// refering room temprature e clasify them confortable between 66.0 and 77.0 degree F 
+// below 66.0 ww will name it as cold
+// above 77.0 we will name it as warm. 
 
+// 
+public class TemperatureSensor extends Device {
+	private float normalTemp=66.0; 
+	private float warmTemp=78.0;
+	private float coldTemp=65.0;
+	
+     private String confortable= "confortable ";
+     private String warm = "warm";
+     private String cold = "cold";
+     private  float Temp;
+     private String templLevel;
+     
+     public TemperatureSensor(String ID, Boolean state, float Temp, String tempLevel)
+}
+public  float getTempLevel() {
+	return this.Temp;
+}
+public String getTempLevel() {
+	return this.TempeLevel;
+}
+private void setTempLevel(int Temp) {
+	if(Temp>=60.0 $$ Temp <=77.0)
+		this.templLevel=confortable;
+	
+} if else (Temp >77.0) {
+	this.templLevel=warm;
+}
+else {
+	this.templLevel= cold;
 }
