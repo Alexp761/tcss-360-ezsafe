b37336e0ebde20c9197f63747c29b8bf111c8d9a
Alec Mac <alecmac95@gmail.com>
2021-03-03 13:30:01 -0800

    added basic base station methods

--- src/main/java/com/threesixty/ezsafe/BaseStation.java
+++ src/main/java/com/threesixty/ezsafe/BaseStation.java
@@ -1,8 +1,11 @@
 package com.threesixty.ezsafe;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BaseStation {
 	
-	final List<Device> deviceList = new ArrayList<>();
+	private final List<Device> deviceList = new ArrayList<>();
 	private int pin;
 	private String userID;
 	private String armState;
@@ -11,10 +14,40 @@ public class BaseStation {
 	// need data connect: object
 	// need powered? boolean
 	
-	private int pin;
-	final List<Device> deviceList = new ArrayList<>();
 	
     public BaseStation() {
-    	// test
+    	/// default pin
+    	pin = 0000;
+    	userID = null;
+    	armState = "UNARMED";
+    	deviceID = "000001";
+    	wifiState = false;
     }
+    
+    public void activateDevice(Device givenDevice) {
+    	deviceList.add(givenDevice);
+    }
+    
+    // returns true if device found and deleted
+    public boolean deactivateDevice(Device givenDevice) {
+    	return deviceList.remove(givenDevice);
+    }
+    
+    public List<Device> getSyncedDevices() {
+    	return deviceList;
+    }
+    
+    public void changeArmState(String armState) {
+    	this.armState = armState;
+    }
+    
+    public void callEmergencyService(String emergencyService) {
+    	// activate emergency service
+    }
+    
+    public void callControlCenter() {
+    	// activate control center
+    
+    }
+    
 }
