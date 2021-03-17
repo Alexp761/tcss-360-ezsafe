f2a08932dcafd5e98711c4029a21e9bc9040d90b
Alec Mac <alecmac95@gmail.com>
2021-03-17 13:36:34 -0700

    Squashed commit of the following:
    commit cf449c40b4e8899b12b1f2ddc27e17d0367046bf
    Author: Alec Mac <alecmac95@gmail.com>
    Date:   Wed Mar 17 13:32:37 2021 -0700
    
        sp
    
    commit 5880457853b13e5078b45f79ce88f80149f23a0e
    Author: Alec Mac <alecmac95@gmail.com>
    Date:   Wed Mar 17 13:29:45 2021 -0700
    
        temperature sp
    
    commit 239db829e94097159e335c68593eab258614f037
    Author: mttademu <65985658+mttademu@users.noreply.github.com>
    Date:   Wed Mar 17 12:36:36 2021 -0700
    
        update
    
    commit 9588853781dd8c25654f1c2287fed68e11e529d2
    Author: mttademu <65985658+mttademu@users.noreply.github.com>
    Date:   Wed Mar 17 12:33:21 2021 -0700
    
        last update on Temperature sensor
    
    commit b4a28d890bae9380cf681ba94c201f41fdbfba96
    Author: mttademu <65985658+mttademu@users.noreply.github.com>
    Date:   Wed Mar 17 06:11:32 2021 -0700
    
        commit
    
    commit ab970c42a50fbf38b2f52fc6ab74c3fc16babed4
    Author: mttademu <65985658+mttademu@users.noreply.github.com>
    Date:   Wed Mar 17 06:09:41 2021 -0700
    
        new update
    
    commit a2a1004bcb761e99471439e390fad4b91ce38e8b
    Author: mttademu <65985658+mttademu@users.noreply.github.com>
    Date:   Wed Mar 17 06:08:56 2021 -0700
    
        updated Temperature sensor
    
    commit 97ebf2af369d605348da24314e47761bbef4dc22
    Author: mttademu <65985658+mttademu@users.noreply.github.com>
    Date:   Wed Mar 17 06:06:45 2021 -0700
    
        updated Temperature Sensor
    
    commit 6ec13ec7103cdd3f5ff199ecc3b083796a9e43bf
    Author: mttademu <65985658+mttademu@users.noreply.github.com>
    Date:   Wed Mar 17 06:02:43 2021 -0700
    
        updated the temperature sensor
    
    commit 58d87ee5c70b7e328cda6bd386a361bc7d923db9
    Author: mttademu <65985658+mttademu@users.noreply.github.com>
    Date:   Sun Mar 14 16:01:56 2021 -0700
    
        updated the temperature class
    
    commit 89611b86866877b18d743ad3145fc9bc0b497c6f
    Author: mttademu <65985658+mttademu@users.noreply.github.com>
    Date:   Sun Mar 14 16:00:30 2021 -0700
    
        michael updated the temprature class

--- src/main/java/com/threesixty/ezsafe/Hazard/TemperatureSensor.java
+++ src/main/java/com/threesixty/ezsafe/Hazard/TemperatureSensor.java
@@ -1,62 +1,90 @@
 package com.threesixty.ezsafe.Hazard;
 
-//Attributes:
+import java.util.*;
 
-//DeviceID:String
-//Temperature:Float
-//On/Off:Boolean
-//TemperatureLevel:String
+/**
+ *
+ * @author Michael Tademu
+ * 
+ * 
+ * 
+ */
 
-//Methods:
+public class TemperatureSensor {
+    private String deviceID;
+    private double Temperature;
+    private boolean onOff;
+    private String TemperatureLevel;
 
-//getDeviceID():String
-//getDeviceState:Boolean
-//getTempLevel()::String
+    public TemperatureSensor(String deviceID, double Temperature, boolean onOff) {
+        this.deviceID = deviceID;
+        this.Temperature = Temperature;
+        this.onOff = onOff;
+    }
 
-//turnOnOff()
+    public String getDeviceID() {
+        return deviceID;
+    }
 
-//changeTempLevel(Temperature) 
-//changeTemp(Degree)    
-//    (Temperature level: Low and dangerous)
+    public String getTemperatureLevel() {
+        return TemperatureLevel;
+    }
 
-public class TemperatureSensor extends Device {
-	private final float normal;
-	private final float low;
-	private float Dangerous;
+    public boolean getDeviceState() {
+        return onOff;
+    }
 
-	private final String normal = "Normal ";
-	private final String Dangerous = "Dangerous";
-	private final String low = "lowl";
+    public void turnOnOff() {
+        if (onOff) {
+            onOff = false;
+        } else {
+            onOff = true;
+        }
+    }
 
-	private float Temp;
-	private String templLevel;
+    public void changeTempLevel(double Temperature) {
+        if (Temperature < 41) {
+            this.TemperatureLevel = " Low";
+        } else if (Temperature > 300) {
+            this.TemperatureLevel = "Dengerous";
+        } else {
+            this.TemperatureLevel = "normal";
 
-	public TemperatureSensor(String ID, Boolean state, float Temp, String tempLevel){
-	   super.device ID = ID;
-	   super.deviceState = state;
-	   this.Temp = Temp;
-	   this. tempLevel = tempLevel
-	    
-	     
-}super.deviceID=ID;super.deviceState=state;this.Temp=Temp;this.tempLevel=tempLevel;
+        }
+    }
 
-	changeTempLevel(Temp,tempLevel);
-	}
+    public void changeTemp(double degree) {
+        this.Temperature = Temperature;
+    }
 
-public  String getTempLevel() {
-	changeTempLevel(this.Temp, this.templLevel)
-	return this.templLevelemp;
-}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-private void changeTempLevel(int Temp) {
-	if(Temp>=41.0 $$ Temp <=300.0)
-		this.templLevel=normal;
-	
-}if else(Temp>=300.0)
+        String id;
+        double Temperature;
 
-	{
-		this.templLevel = Dangerous;
-	}else
-	{
-	this.templLevel= low;
+        System.out.println("Enter device ID: ");
+        id = sc.nextLine();
+        System.out.println("Enter device Temperature: ");
+        Temperature = sc.nextDouble();
+        System.out.println("Enter device status (ON/OFF): ");
+        String status = sc.nextLine();
+        if (status.equals("")) {
+            status = sc.nextLine();
+
+        }
+        boolean onOff;
+        if (status.toLowerCase().equals("ON")) {
+            onOff = true;
+        } else {
+            onOff = false;
+        }
+
+        TemperatureSensor d = new TemperatureSensor(id, Temperature, onOff);
+
+        d.changeTempLevel(Temperature);
+
+        System.out.println("Device Level: " + d.getTemperatureLevel());
+
+    }
 }
